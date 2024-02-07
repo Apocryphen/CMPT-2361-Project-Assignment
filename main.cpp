bc3e24fecad607d7da0caf59e0fd06e0a93e5838
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-07 15:42:25 -0800

    Setup file reading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(){
+using std::string;
+
+int main(int argc, char** argv){
+    string fileName = "example.ppm"; // Default filename
+    if (argc == 2) fileName = argv[1];
+
+    std::ifstream infile(fileName);
+
+    if(!infile.is_open()) {
+        std::cerr << "Failed to open file " << fileName << std::endl;
+        return 1;
+    }
+
+    while (infile){
+        int test;
+        infile >> test;
+        std::cout << "Got: " << test << std::endl;
+    }
+
+    infile.close();
     return 0;
 }
