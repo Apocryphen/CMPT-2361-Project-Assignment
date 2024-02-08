4f86d137c8c9a783714bd2bcc9d597228015d2e4
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-07 17:45:27 -0800

    Remove test case and change default file name

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "ppm.hpp"
 
 using std::string;
 
 int main(int argc, char** argv){
-    string fileName = "example.ppm"; // Default filename
+    string fileName = "ex.ppm"; // Default filename
     if (argc == 2) fileName = argv[1];
 
     std::ifstream infile(fileName);
@@ -13,12 +14,6 @@ int main(int argc, char** argv){
     if(!infile.is_open()) {
         std::cerr << "Failed to open file " << fileName << std::endl;
         return 1;
-    }
-
-    while (infile){
-        int test;
-        infile >> test;
-        std::cout << "Got: " << test << std::endl;
     }
 
     infile.close();
