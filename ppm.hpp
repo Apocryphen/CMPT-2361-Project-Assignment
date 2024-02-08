c944958cb870e937cc274e9b782c70f31569f647
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-07 17:48:29 -0800

    Add PPM header with base decls

--- ppm.hpp
+++ ppm.hpp
@@ -1,12 +1,49 @@
 #ifndef PPM_HPP
 #define PPM_HPP
 
-class Pixel {
+#include <iostream> // For friend decls
 
+class Pixel {
+    public:
+        Pixel();
+        Pixel(const Pixel& other);
+        Pixel(Pixel&& other);
+        Pixel(unsigned int r, unsigned int g, unsigned int b);
+        ~Pixel();
+
+        const unsigned int& operator[](const char* colour) const;
+
+        Pixel& operator=(const Pixel& other);
+        Pixel& operator=(Pixel&& other);
+
+        friend std::ostream& operator<<(std::ostream& out, const Pixel& pixel);
+        friend std::istream& operator>>(std::istream& in, Pixel& pixel);
+
+    private:
+        unsigned int red, green, blue;
 };
 
 class PPMImage {
+    public:
+        PPMImage();
+        PPMImage(const PPMImage& other);
+        PPMImage(PPMImage&& other);
+        ~PPMImage();
 
+        PPMImage& operator=(const PPMImage& other);
+        PPMImage& operator=(PPMImage other);
+        PPMImage& operator=(PPMImage&& other);
+
+        friend std::ostream& operator<<(std::ostream& out, const PPMImage& pixel);
+        friend std::istream& operator>>(std::istream& in, PPMImage& pixel);
+
+        friend void swap(PPMImage& a, PPMImage& b);
+
+    private:
+        std::vector<Pixel> pixels;
+        std::string format;
+        unsigned int width, height;
+        unsigned int channelDepth;
 };
 
 #endif
