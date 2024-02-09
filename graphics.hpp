c786fdbc1f670def2174247712061820bc62cac4
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-09 14:26:34 -0800

    Added required feilds to graphics header

--- graphics.hpp
+++ graphics.hpp
@@ -1,8 +1,14 @@
 #ifndef GRAPHICS_HPP
 #define GRAPHICS_HPP
 
-class Graphics{
+#include "ppm.hpp"
 
+class Graphics{
+    static const PPM& ApplyFilter(PPM& image, const char* filter);
+    static const PPM& MakeGreyScale(PPM& image);
+    static const PPM& RotateImage(PPM& image);
+    static const PPM& ScaleImage(PPM& image);
+    static const PPM& TranslateImage(PPM& image);
 };
 
 #endif
