1b6d067c4bb799ca00d218e55adb2c3ed8cb4b9b
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-10 01:20:31 -0800

    Revise graphics header decls, typos were made

--- graphics.hpp
+++ graphics.hpp
@@ -4,11 +4,12 @@
 #include "ppm.hpp"
 
 class Graphics{
+    public:
     static const PPM& ApplyFilter(PPM& image, const char* filter);
     static const PPM& MakeGreyScale(PPM& image);
-    static const PPM& RotateImage(PPM& image);
-    static const PPM& ScaleImage(PPM& image);
-    static const PPM& TranslateImage(PPM& image);
+    static const PPM& RotateImage(PPM& image, double angle);
+    static const PPM& ScaleImage(PPM& image, double factor);
+    static const PPM& TranslateImage(PPM& image, int x, int y);
 };
 
 #endif
