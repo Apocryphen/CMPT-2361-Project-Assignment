8b481742049bf214fa2fcfe2629b9d40f088039d
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-10 15:50:00 -0800

    Port Makegreyscale over from previous tutorial

--- graphics.cpp
+++ graphics.cpp
@@ -1,1 +1,12 @@
 #include "graphics.hpp"
+#include <algorithm>
+
+Pixel pixelAverage(Pixel p){
+    int avg = (p["red"] + p["green"] + p["blue"]) / 3;
+    return Pixel(avg);
+}
+
+const PPM& Graphics::MakeGreyScale(PPM& image){
+    std::transform(image.begin(), image.end(), image.begin(), pixelAverage);
+    return image;
+}
