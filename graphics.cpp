2ec3f5ee5158ca6e50bca4c97f45fb6b118281fe
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-12 15:58:51 -0800

    Implement image translation

--- graphics.cpp
+++ graphics.cpp
@@ -38,3 +38,34 @@ const PPM& Graphics::ScaleImage(PPM& image, double factor){
     image = std::move(resized);
     return image;
 }
+
+const PPM& Graphics::TranslateImage(PPM& image, int x, int y){
+    Pixel blackPixel;
+    const unsigned int imageSize   = image.GetSize(),
+                       imageWidth  = image.GetWidth();
+    const bool shiftLeft = x <= 0,
+               shiftUp   = y <= 0;
+
+    int xPivotIdx = (shiftLeft ? 0 : imageSize) - x; //Select the top left or bottom right of the image
+    int yPivotIdx = (shiftUp   ? 0 : imageSize) - (y * imageWidth); //Select the top or bottom row of the image
+
+    auto xPivot = image.begin() + xPivotIdx;
+    auto yPivot = image.begin() + yPivotIdx;
+    auto pivot  = image.begin() + (xPivotIdx + yPivotIdx) % imageSize;
+
+    // This is for blacking out the background, if you want the image to wrap then discard these fills
+    // Note that image.begin() is the bottom right pixel and image.end() is the top left after the roatate
+    
+    if(shiftUp) std::fill(image.begin(), yPivot, blackPixel); //Fill from the top of the frame to the top of the image
+    else        std::fill(yPivot,   image.end(), blackPixel); //Fill from the bottom of the frame to the bottom of the image
+
+    for(int row = 0; row < imageSize; row += imageWidth){
+        if(shiftLeft) std::fill(image.begin() + row, xPivot + row, blackPixel); //Fill from the right of the image to the right of the frame
+        else          std::fill(xPivot - row,   image.end() - row, blackPixel); //Fill from the left of the image to the left of the frame
+    }
+
+    std::rotate(image.begin(), pivot, image.end()); // Move the pivot (top or bottom corner of the image) to the bottom right of the frame,
+                                                    // wrapping the rest around, the wrapping gives the same effect as moving in the opposite direction
+
+    return image;
+}
