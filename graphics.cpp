d52ffe0bc0b35aafaab6399c05eda899bc4ef3ec
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-12 15:58:22 -0800

    Implement image scaling

--- graphics.cpp
+++ graphics.cpp
@@ -10,3 +10,31 @@ const PPM& Graphics::MakeGreyScale(PPM& image){
     std::transform(image.begin(), image.end(), image.begin(), pixelAverage);
     return image;
 }
+
+const PPM& Graphics::ScaleImage(PPM& image, double factor){
+    PPM resized;
+    int imageWidth = image.GetWidth(),
+        imageHeight = image.GetHeight(),
+        centerCol = imageWidth / 2,
+        centerRow = imageHeight / 2;
+
+    //Manually set the metadata to avoid a full array copy.
+    resized.SetMetaData(image);
+    resized.Reserve(image.GetSize());
+
+    auto pivotedScale = [&factor](int center, int value){
+        return center + static_cast<int>((value - center) / factor);
+    };
+
+    for(int row = 0; row < imageHeight; row++){
+        for(int col = 0; col < imageWidth;  col++){
+            int dest = row * imageWidth + col;
+            int src = pivotedScale(centerRow, row) * imageWidth +
+                      pivotedScale(centerCol, col);
+            resized[dest] = image[src];
+        }
+    }
+
+    image = std::move(resized);
+    return image;
+}
