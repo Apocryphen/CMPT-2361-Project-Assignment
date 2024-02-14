06f8251adc2aea9660d7840949fc59506076b39b
Apocryphen <65224279+Apocryphen@users.noreply.github.com>
2024-02-13 23:40:25 -0800

    Update main so that it logs the time it took for each function call

--- main.cpp
+++ main.cpp
@@ -1,76 +1,95 @@
 #include <cstdlib>
+#include <chrono>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "ppm.hpp"
 #include "graphics.hpp"
 
+const std::string outputFolder = "./output/";
+
+void applyFilterDemo(PPM baseImage){
+    //TODO
+}
+
+void rotateDemo(PPM baseImage){
+    Graphics::RotateImage(baseImage, 3.1415f/6);
+    baseImage.SaveImageToFile(outputFolder + "rotateDemo.ppm");
+}
+
 void translateDemo(PPM baseImage){
-    std::string demoName = "translateDemo.ppm";
-    std::ofstream outfile(demoName);
-    if(!outfile.is_open()){
-        std::cerr << "Failed to open file " << demoName << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
     Graphics::TranslateImage(baseImage, 50, 100);
-
-    outfile << baseImage;
-    outfile.close();
+    baseImage.SaveImageToFile(outputFolder + "translateDemo.ppm");
 }
 
 void scaleDemo(PPM baseImage){
-    std::string demoName = "scaleDemo.ppm";
-    std::ofstream outfile(demoName);
-    if(!outfile.is_open()){
-        std::cerr << "Failed to open file " << demoName << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
     Graphics::ScaleImage(baseImage, 0.5);
-
-    outfile << baseImage;
-    outfile.close();
+    baseImage.SaveImageToFile(outputFolder + "scaleDemo.ppm");
 }
 
 void greyscaleDemo(PPM baseImage){
-    std::string demoName = "greyscaleDemo.ppm";
-    std::ofstream outfile(demoName);
-    if(!outfile.is_open()){
-        std::cerr << "Failed to open file " << demoName << std::endl;
+    Graphics::MakeGreyScale(baseImage);
+    baseImage.SaveImageToFile(outputFolder + "greyscaleDemo.ppm");
+}
+
+PPM loadFile(std::string filename){
+    std::ifstream infile(filename);
+
+    if(!infile.is_open()) {
+        std::cerr << "Failed to open file " << filename << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    Graphics::MakeGreyScale(baseImage);
-
-    outfile << baseImage;
-    outfile.close();
+    PPM loadedImage;
+    try{
+        loadedImage = PPM(infile);
+    }
+    catch(const std::runtime_error& e){
+        std::cerr << e.what() << std::endl;
+        infile.close();
+        exit(EXIT_FAILURE);
+    }
+    infile.close();
+    return loadedImage;
 }
+
+template<typename... Ts>
+void timeFunction(std::string desc, void fn(Ts...), Ts... args){
+    auto pre = std::chrono::high_resolution_clock::now();
+    fn(args...);
+    auto post = std::chrono::high_resolution_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(post - pre);
+    std::cout << desc << " operation took " << diff.count() << " (ms)" << std::endl;
+}
+
+template<typename R, typename... Ts>
+R timeFunction(std::string desc, R fn(Ts...), Ts... args){
+    auto pre = std::chrono::high_resolution_clock::now();
+    R result = fn(args...);
+    auto post = std::chrono::high_resolution_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(post - pre);
+    std::cout << desc << " operation took " << diff.count() << " (ms)" << std::endl;
+    return result;
+}
+
 
 int main(int argc, char** argv){
     std::string fileName = "ex.ppm"; // Default filename
     if (argc >= 2) fileName = argv[1];
 
-    std::ifstream infile(fileName);
+    PPM loadedImage = timeFunction("Load image", loadFile, fileName);
 
-    if(!infile.is_open()) {
-        std::cerr << "Failed to open file " << fileName << std::endl;
-        return EXIT_FAILURE;
+    try {
+        timeFunction("Convert to greyscale", greyscaleDemo, loadedImage);
+        timeFunction("Scale image", scaleDemo, loadedImage);
+        timeFunction("Translate image", translateDemo, loadedImage);
+        timeFunction("Roatate image", rotateDemo, loadedImage);
+        timeFunction("Filter image", applyFilterDemo, loadedImage);
     }
-
-    PPM loadedImage;
-    infile >> loadedImage;
-
-    if(infile.fail()){
-        std::cerr << "Could not parse PPM File" << std::endl;
-        infile.close();
-        return EXIT_FAILURE;
+    catch(const std::runtime_error& e){
+        std::cerr << e.what() << std::endl;
+        exit(EXIT_FAILURE);
     }
-    infile.close();
-    
-    greyscaleDemo(loadedImage);
-    scaleDemo(loadedImage);
-    translateDemo(loadedImage);
 
     return EXIT_SUCCESS;
 }
